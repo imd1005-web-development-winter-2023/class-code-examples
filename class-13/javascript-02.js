7c1cf32fa0b960fd46e824be52326aacbac52cf4
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-03-01 21:50:57 -0500

    Add js code samples

---
+++ class-13/javascript-02.js
@@ -0,0 +1,47 @@
+// Declare a variable
+let message = "Hello Worldsdfsd";
+
+// Console log the contents of the variable
+console.log(message);
+
+let numberOfStudents = 5;
+
+let stringOfStundents = "2";
+
+let result = numberOfStudents + stringOfStundents;
+
+console.log(result);
+
+let isPizzaHere = false;
+
+console.log("Pizza status: ", isPizzaHere);
+
+let orderPizza = null;
+
+console.log("Pizza order: ", orderPizza);
+
+let elephantName = "John";
+let elephantAge = 20;
+
+console.log(elephantAge, elephantName);
+
+let elephant = {
+  name: "John",
+  age: 20,
+};
+
+console.log(elephant);
+
+console.log("Elephant age:", elephant.age);
+
+console.log("Elephant name:", elephant.name);
+
+let todoItem = {
+  title: "Some title",
+  description: "Long description",
+  isDone: false,
+};
+
+console.log(todoItem);
+
+console.log(todoItem.title);
