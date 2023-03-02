d1b35c37e00bfc5244f6b3acafe6325f75f53297
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-03-02 14:24:13 -0500

    Add changes to array code

--- class-14/array-01.js
+++ class-14/array-01.js
@@ -112,3 +112,85 @@ starWarsShips[0] = "TIE Advanced x1";
 // Output the array -
 // ("TIE Advanced x1", "Star Destroyer", "Millennium Falcon", "CR90 corvette", "X-Wing");
 console.log(starWarsShips);
+
+console.clear();
+
+const myLotteryNumberss = [1, 22, 34, 56, 83, 59, 23];
+
+// Output the array
+console.log("Original: ", myLotteryNumberss);
+
+// Reverse method
+myLotteryNumberss.reverse();
+
+// Output the array
+console.log("After reverse: ", myLotteryNumberss);
+
+console.clear();
+
+let a = false;
+
+let b = a;
+
+b = true;
+
+console.log("a", a);
+console.log("b", b);
+
+let aArray = [1, 2, 3, 4];
+
+let bArray = [...aArray];
+
+bArray[0] = 100;
+
+console.log("a", aArray);
+console.log("b", bArray);
+
+//number % 2 == 0
+console.clear();
+
+function isEven(num) {
+  if (num % 2 == 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+}
+
+isEven(300); // Even
+isEven(55); // (Blank)
+isEven(44); // Even
+isEven(99); // (Blank)
+isEven(22); // Even
+isEven(2400); // Even
+
+console.clear();
+
+const rating = 4;
+
+if (rating === 5) {
+  /* If rating is 5 */
+  console.log("Excellent job");
+} else if (rating === 4) {
+  /* If rating is 4 */
+  console.log("That's really wonderful");
+} else if (rating === 3) {
+  /* If rating is 3 */
+  console.log("Great job");
+} else if (rating === 2) {
+  /* If rating is 2 */
+  console.log("Awh no, that was a good try");
+} else if (rating === 1) {
+  /* If rating is 1 */
+  console.log("Ummm, so this is awkward");
+}
+
+console.clear();
+
+const mystery = "S00A08900";
+
+if (mystery[0] === "S" && mystery.length > 4 && mystery.indexOf("A") !== -1) {
+  console.log("YOU GOT IT!");
+} else {
+  console.log("Keep trying, you can do it!");
+}
