56d487dcf938ec8795168d5008c18e5780048675
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-03-09 01:29:26 -0500

    Add class 16 material

---
+++ class-16/section-b/main.js
@@ -0,0 +1,52 @@
+//
+//
+// events-add-event-listner
+//
+//
+
+// Three ways of adding events to an element
+// addEventListener()
+// inline onclick()
+// and the onlick property
+
+//
+//
+// events-objects
+//
+//
+
+// Add an event listener and console log the event object
+
+//
+//
+// events-forms
+//
+//
+
+// Cache all of the dom items
+// Add an event listener on form submit
+// Grab the form data
+// Create a new li
+// Add the list item into the parent ul
+
+//
+//
+// events-bubbling
+//
+//
+
+// Cache all of the four bubble elements and the button
+// Add click listeners to all of them
+// Show how stopPropogation() stops the event from bubbling
+
+//
+//
+// events-delegation
+//
+//
+
+// Neil don't forget - event.target.dataset.todo;
+
+// Cache the dom objects #list-2 #message-1
+// Create the list hander
+// Add the event listeners
